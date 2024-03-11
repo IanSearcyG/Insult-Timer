bccede0c0b51cfcb391e7bf6623805016d391bcd
Ian Searcy-Gardner <ian@searcygardner.com>
2024-03-10 20:16:37 -0700

    Initial Commit

---
+++ Insult Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Insult Timer
+//
+//  Created by Ian Searcy-Gardner on 3/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Insult Timer/Insult_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insult_TimerApp.swift
+//  Insult Timer
+//
+//  Created by Ian Searcy-Gardner on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Insult_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
